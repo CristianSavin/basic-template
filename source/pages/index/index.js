83f42d7230586a1c76ac609db6771a3cee9fb2f3
Cristian Savin <savin.cristian95@icloud.com>
2018-01-24 15:02:00 +0200

    add normalize.css

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import 'normalize.css';
 import createMenu from '../../components/menu/menu';
 var menu = createMenu(['Principala' , 'Blog'], 'menu');
 document.body.appendChild(menu);
